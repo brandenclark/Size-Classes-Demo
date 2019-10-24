bb5c9300276ec8b22e4e60d839e58d5b563b1ebf
Branden Clark <brandenrayclark@gmail.com>
2019-10-24 08:18:01 -0600

    Initial Commit

---
+++ Size-Classes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Size-Classes
+//
+//  Created by Branden Clark on 10/24/19.
+//  Copyright © 2019 Branden Clark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
